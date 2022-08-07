454d5e2ea0d6e3896f1baad6ddd8a696f1563a8c
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-08-07 09:50:52 -0300

    layout finalizado

--- src/Components/Rodape.js
+++ src/Components/Rodape.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { Link } from "react-router-dom";
 
 export default function Rodape(){
-    const percentage = 66;
+    const percentage = 65;
     return (
         <RodapeNavegador>
             <RodapeTela>
                 <Link to={"/habitos"}><p>Hábitos</p></Link>
-                {/* <CircularProgressbar value={percentage} text={`${percentage}%` } />; */}
-                <Link to={"/hoje"}><p>Hoje</p></Link>
+                <Link to={"/hoje"}><IconeHoje>
+                    <CircularProgressbar
+                        value={percentage}
+                        text='Hoje'
+                        styles={buildStyles({
+                            rotation: 0,
+                            strokeLinecap: 'round',
+                            textSize: '16px',
+                            pathTransitionDuration: 0.5,
+                            pathColor: `#FFFFFF`,
+                            textColor: '#FFFFFF',
+                            textSize: '22px',
+                            trailColor: '#52B6FF',
+                            backgroundColor: '#3e98c7',
+                        })}
+                    />;
+                </IconeHoje></Link>
                 <Link to={"/historico"}><p>Histórico</p></Link>
             </RodapeTela>
         </RodapeNavegador>
@@ -41,10 +56,16 @@ const RodapeTela = styled.div`
     align-items: center;
     color: #52B6FF;
     font-size: 18px;
+    position: relative;
 
     a:-webkit-any-link {
         text-decoration: none;
         color: inherit;
+    }
+
+    p{
+        padding-right: 18px;
+        padding-left: 18px;
     }
 `;
 
@@ -54,3 +75,12 @@ const Perfil = styled.img`
     width: 51px;
     border-radius: 25px;
     `;
+
+const IconeHoje = styled.div`
+background-color: #52B6FF;
+width: 75px;
+height: 75px;
+border-radius: 60px;
+padding: 6px;
+margin-bottom: 50px;
+`
